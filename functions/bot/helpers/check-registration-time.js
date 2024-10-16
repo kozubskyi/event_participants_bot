a2bf5bbc1175241a19066eeee30cf0b542baf464
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2024-10-16 18:51:42 +0300

    registration time check done

--- functions/bot/helpers/check-registration-time.js
+++ functions/bot/helpers/check-registration-time.js
@@ -1,4 +1,3 @@
-const formatDate = require('./format-date')
 const getFullName = require('./get-full-name')
 
 module.exports = async function checkRegistrationTime(ctx, event) {
@@ -8,19 +7,12 @@ module.exports = async function checkRegistrationTime(ctx, event) {
 	const kyivOffset = 3 * 60 * 60 * 1000 // 3 часа в миллисекундах
 	const nowInKyiv = new Date(now.getTime() + kyivOffset - now.getTimezoneOffset() * 60 * 1000)
 
-	await ctx.reply(`${now}\n${nowInKyiv}`)
-	// const nowLocaleString = new Date().toLocaleString('uk-UA', { timeZone: 'Europe/Kyiv' })
-	// const now = new Date(formatDate(nowLocaleString))
-
 	if (registrationStart) {
 		const [date, time] = registrationStart.split(', ')
 		const [day, month, year] = date.split('.')
 		const [hours, minutes] = time.split(':')
 
-		// const registrationStartDate = new Date(formatDate(registrationStart))
 		const registrationStartDate = new Date(year, month - 1, day, hours, minutes)
-
-		await ctx.reply(`${registrationStartDate}`)
 
 		if (nowInKyiv < registrationStartDate) {
 			await ctx.replyWithHTML(`<b>${getFullName(ctx)}</b>, період реєстрації ще не розпочався.`)
@@ -33,10 +25,8 @@ module.exports = async function checkRegistrationTime(ctx, event) {
 		const [day, month, year] = date.split('.')
 		const [hours, minutes] = time.split(':')
 
-		// const registrationEndDate = new Date(formatDate(registrationEnd))
 		const registrationEndDate = new Date(year, month - 1, day, hours, minutes)
 
-		// console.log({ now, nowInKyiv, registrationEndDate })
 		await ctx.reply(`${registrationEndDate}`)
 
 		if (nowInKyiv > registrationEndDate) {
@@ -44,14 +34,6 @@ module.exports = async function checkRegistrationTime(ctx, event) {
 			return false
 		}
 	}
-	// else {
-	// 	const startDate = new Date(formatDate(start))
-
-	// 	if (now > startDate) {
-	// 		await ctx.replyWithHTML(`<b>${getFullName(ctx)}</b>, подія вже розпочалася, тому реєстрація закрита.`)
-	// 		return false
-	// 	}
-	// }
 
 	return true
 }
