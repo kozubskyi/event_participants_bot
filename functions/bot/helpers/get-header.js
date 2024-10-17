a3c4967763dab02ebb9719aad2dce2b8d759bd08
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2024-10-17 16:22:37 +0300

    add event description and location

--- functions/bot/helpers/get-header.js
+++ functions/bot/helpers/get-header.js
@@ -1,14 +1,24 @@
 module.exports = function getHeader(updatedEvent) {
-	const { title, start, end, registrationStart, registrationEnd, reserveDeadline, participantsMin, participantsMax } =
-		updatedEvent
+	const {
+		title,
+		description,
+		location,
+		start,
+		end,
+		registrationStart,
+		registrationEnd,
+		reserveDeadline,
+		participantsMin,
+		participantsMax,
+	} = updatedEvent
 
 	return `
 <b>${title}</b>
-Початок: ${start}${end ? `\nКінець: ${end}` : ''}${
-		registrationStart ? `\nПочаток реєстрації: ${registrationStart}` : ''
-	}${reserveDeadline ? `\nРезерв до: ${reserveDeadline}` : ''}${
-		registrationEnd ? `\nКінець реєстрації: ${registrationEnd}` : ''
-	}${participantsMin ? `\nМінімум учасників: ${participantsMin}` : ''}${
-		participantsMax ? `\nМаксимум учасників: ${participantsMax}` : ''
-	}`
+${description ? `Опис: ${description}\n` : ''}${location ? `Локація: ${location}\n` : ''}Початок: ${start}${
+		end ? `\nКінець: ${end}` : ''
+	}${registrationStart ? `\nПочаток реєстрації: ${registrationStart}` : ''}${
+		reserveDeadline ? `\nРезерв до: ${reserveDeadline}` : ''
+	}${registrationEnd ? `\nКінець реєстрації: ${registrationEnd}` : ''}${
+		participantsMin ? `\nМінімум учасників: ${participantsMin}` : ''
+	}${participantsMax ? `\nМаксимум учасників: ${participantsMax}` : ''}`
 }
