9aaa1379cc5d0b06c33dceefb2c385e24713cd8b
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2025-04-20 09:41:47 +0300

    replace bot from Railway to Netlify

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { Telegraf, Markup } = require('telegraf')
+const { Telegraf } = require('telegraf')
 const handlers = require('./handlers')
 const constants = require('./helpers/constants')
 
@@ -63,4 +63,19 @@ participantsMax: 15`)
 
 run()
 
-bot.launch()
+// bot.launch()
+
+// Enable graceful stop
+// process.once('SIGINT', () => bot.stop('SIGINT'))
+// process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+// AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
+exports.handler = async event => {
+	try {
+		await bot.handleUpdate(JSON.parse(event.body))
+		return { statusCode: 200, body: '' }
+	} catch (e) {
+		console.error('error in handler:', e)
+		return { statusCode: 400, body: 'This endpoint is meant for bot and telegram communication' }
+	}
+}
